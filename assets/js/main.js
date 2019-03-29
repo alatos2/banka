1790508bef2345025cf2052e6ddafc68cd3abe80
alatos2 <alabitosin60@gmail.com>
2019-03-30 00:19:39 +0100

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,7 @@
     document.getElementById("btn-sign-in").onclick = function () {
         location.href = "dashboard.html";
     };
+
+    document.getElementById("btn-sign-up").onclick = function () {
+        location.href = "signin.html";
+    };
