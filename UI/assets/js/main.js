d50c98bbe37a53f902227386a12de4954f48a8f8
alabi tosin <alabitosin60@gmail.com>
2019-03-28 20:51:48 -0700

    Initialize Repo

---
+++ UI/assets/js/main.js
@@ -0,0 +1,34 @@
+// const activate = document.getElementById("activate");
+
+// activate.addEventListener("click", () => {
+// 	const answer = prompt("Do you want to activate this account? y/n");
+// 	if(answer === 'y' || answer === 'yes'){
+// 		alert("Account activated.");
+// 	} else {
+// 		alert("Account not activated.");
+// 	}
+
+// });
+
+// const deactivate = document.getElementById("deactivate");
+
+// deactivate.addEventListener("click", () => {
+// 	const answer = prompt("Do you want to deactivate this account? y/n");
+// 	if(answer === 'y' || answer === 'yes'){
+// 		alert("Account deactivated.");
+// 	} else {
+// 		alert("Account not deactivated.");
+// 	}
+// });
+
+// const delete = document.getElementById("delete");
+
+// delete.addEventListener("click", () => {
+// 	const answer = prompt("Do you want to delete this account? y/n");
+// 	if(answer === 'y' || answer === 'yes'){
+// 		alert("Account deleted.");
+// 	} else {
+// 		alert("Account not deleted.");
+// 	}
+// });
+
