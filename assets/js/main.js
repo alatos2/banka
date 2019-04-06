48d4deca6f215e22bafd050db75ee11849f5aeb8
alabi tosin <alabitosin60@gmail.com>
2019-04-06 07:44:12 -0700

    javascript updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 
-    document.getElementById("btn-sign-in").onclick = function () {
-        location.href = "dashboard.html";
-    };
+    //get modal element
+    const modal = document.getElementById("simpleModal");
+    //get the open modal btn
+    const modalBtn = document.getElementById("modalBtn");
+    // get close button
+    const closeBtn = document.getElementsByClassName("closeBtn")[0];
 
+    const openModal = () => {
+      modal.style.display = "block";
+    }
+
+    const closeModal = () => {
+      modal.style.display = "none";
+    }
+
+    const clickOutside = (e) => {
+      if(e.target == modal){
+        modal.style.display = "none";
+      }
+    }
+    
+    //click event
+    modalBtn.addEventListener("click", openModal);
+    closeBtn.addEventListener("click", closeModal);
+    window.addEventListener("click", clickOutside)
+
+
+    // for user, staff and admin page menus -> responsiveness
     function myFunction() {
       var x = document.getElementById("myTopnav");
       if (x.className === "topnav") {
@@ -11,4 +35,3 @@
         x.className = "topnav";
       }
     }
-
